4b31236b5c95ec427b743e28b21ad6a428f67295
azerid <antareza08@gmail.com>
2020-07-29 19:22:45 +0700

    Commit activity detail

--- app/src/main/java/id/azer/minumanapp/MainActivity.java
+++ app/src/main/java/id/azer/minumanapp/MainActivity.java
@@ -2,13 +2,28 @@ package id.azer.minumanapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout = findViewById(R.id.rush);
+
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(),DetailActivity.class);
+                startActivity(in);
+            }
+        });
     }
 }
