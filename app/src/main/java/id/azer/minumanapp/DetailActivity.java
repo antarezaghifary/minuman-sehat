e970bfb11270ad0af61b6236a098b66b0c0f3ff9
azerid <antareza08@gmail.com>
2020-07-30 11:04:06 +0700

    Update Tampilan Home 2

--- app/src/main/java/id/azer/minumanapp/DetailActivity.java
+++ app/src/main/java/id/azer/minumanapp/DetailActivity.java
@@ -2,13 +2,62 @@ package id.azer.minumanapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    Button button;
+    TextView nama,harga,singkat,deskripsi;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        button = findViewById(R.id.back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("tezt","tezt");
+                Intent in = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(in);
+                finish();
+            }
+        });
+
+        nama = findViewById(R.id.nama);
+        harga = findViewById(R.id.harga);
+        imageView = findViewById(R.id.image);
+        singkat = findViewById(R.id.singkat);
+        deskripsi = findViewById(R.id.deskripsi);
+
+        if(getIntent().getExtras()!=null){
+            /**
+             * Jika Bundle ada, ambil data dari Bundle
+             */
+            Bundle bundle = getIntent().getExtras();
+            nama.setText(bundle.getString("nama1"));
+            harga.setText(bundle.getString("harga1"));
+            imageView.setImageResource(getIntent().getIntExtra("imageData",0));
+            singkat.setText(bundle.getString("singkat"));
+            deskripsi.setText(bundle.getString("deskripsi"));
+        }else{
+            /**
+             * Apabila Bundle tidak ada, ambil dari Intent
+             */
+            nama.setText(getIntent().getStringExtra("nama1"));
+            harga.setText(getIntent().getStringExtra("harga1"));
+            imageView.setImageResource(getIntent().getIntExtra("imageData",0));
+            singkat.setText(getIntent().getStringExtra("singkat"));
+            deskripsi.setText(getIntent().getStringExtra("deskripsi"));
+        }
     }
 }
--- app/src/main/java/id/azer/minumanapp/MainActivity.java
+++ app/src/main/java/id/azer/minumanapp/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout linearLayout;
+    LinearLayout linearLayout,linearLayout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +17,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         linearLayout = findViewById(R.id.rush);
+        linearLayout1 = findViewById(R.id.relax);
 
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent in = new Intent(getApplicationContext(),DetailActivity.class);
+                in.putExtra("nama1","Ginger Rush");
+                in.putExtra("harga1", "Rp. 27.000");
+                in.putExtra("imageData",R.drawable.minum2d);
+                in.putExtra("singkat","Perpaduan teh hijau dengan teh");
+                in.putExtra("deskripsi","Minuman jahe, seperti teh jahe atau susu jahe, memang sangat dikenal bermanfaat untuk menghangatkan perut serta mengatasi berbagai keluhan pada perut dan pencernaan, terutama mual. Tanaman ini juga dikenal memiliki kandungan antibiotik alami.");
                 startActivity(in);
+                finish();
+            }
+        });
+
+        linearLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(),DetailActivity.class);
+                in.putExtra("nama1","Relax Green");
+                in.putExtra("harga1", "Rp. 25.000");
+                in.putExtra("imageData",R.drawable.minum1d);
+                in.putExtra("singkat","Perpaduan teh hijau dengan gula madu");
+                in.putExtra("deskripsi","Minuman jahe, seperti teh jahe atau susu jahe, memang sangat dikenal bermanfaat untuk menghangatkan perut serta mengatasi berbagai keluhan pada perut dan pencernaan, terutama mual. Tanaman ini juga dikenal memiliki kandungan antibiotik alami.");
+                startActivity(in);
+                finish();
             }
         });
     }
